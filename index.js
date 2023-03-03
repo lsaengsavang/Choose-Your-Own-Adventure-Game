bdfe97ae244c8674f2c12fa93cd9e902fc9de34d
Leeann Saengsavang <leeann.saengsavang@gmail.com>
2023-03-02 17:32:19 -0800

    Added right prompts

--- index.js
+++ index.js
@@ -7,17 +7,17 @@ Do you follow it, or continue on your path?`)
         if(secondAnswer === 'follow') {
             let thirdAnswer = window.prompt(`You follow the cat to a colony of cats, nestled in a fort of warm blankets and subsisting off of inexplicably warm soup. They are content with you staying, but you wonder if you should alert the world to this magical safe haven.`)
             if(thirdAnswer === 'stay') {
-                let fourthAnswer = window.prompt(`You live happily amongst the cats for the rest of your days.`)
+                let fourthAnswer = window.alert(`You live happily amongst the cats for the rest of your days.`)
             } else if(thirdAnswer === 'spread the word') {
-                let fourthAnswer = window.prompt(`After leaving the cat colony, you are never able to find it again; without proof, no one believes your story, which passes into legend nonetheless.`)
+                let fourthAnswer = window.alert(`After leaving the cat colony, you are never able to find it again; without proof, no one believes your story, which passes into legend nonetheless.`)
             }
 
         } else if(secondAnswer === 'continue') {
             let thirdAnswer = window.prompt(`You come across a chamber that reaches upward to a shining light above. There is a long, winding staircase, and a much quicker, but rickety-looking ladder that leads up toward the light. Which do you take?`)
             if (thirdAnswer === 'ladder') {
-                let fourthAnswer = window.prompt(`After ascending a few feet up the ladder, one of its rungs snaps, and you comedically fall through each of the rungs below. Sheepish, you return home.`)
+                let fourthAnswer = window.alert(`After ascending a few feet up the ladder, one of its rungs snaps, and you comedically fall through each of the rungs below. Sheepish, you return home.`)
             } else if(thirdAnswer === 'staircase') {
-                let fourthAnswer = window.prompt(`After ascending the staircase, you discover a shiny blue stone, which you take home and cherish forever.`)
+                let fourthAnswer = window.alert(`After ascending the staircase, you discover a shiny blue stone, which you take home and cherish forever.`)
             }
 
         } 
@@ -26,5 +26,11 @@ Do you follow it, or continue on your path?`)
     let secondAnswer = window.prompt(`You come across a snoring dragon. 
 On the other side of him, you see a shiny chest of treasure. Another path would 
 lead you away from the dragon altogether. Which path do you take?`)
+        if (secondAnswer === 'past the dragon') {
+            let thirdAnswer = window.prompt(`The dragon wakes up and sits upright. You only have a moment to respond, to stay or run:`)
+        } else if (secondAnswer === 'away from the dragon') {
+            let thirdAnswer = window.prompt(`After walking a while longer, you come across a shiny blue flower. It is so beautiful that you decide you must either draw it or pick it. Which do you do?`)
+        }
+
 
 }
