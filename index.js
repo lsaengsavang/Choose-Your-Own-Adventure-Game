efdb05eaad5b1dbf65911043bf449aa1ab61480b
Leeann Saengsavang <leeann.saengsavang@gmail.com>
2023-03-02 16:57:08 -0800

    Added prompt for left answer

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-window.prompt('Do you head left or right?')
+
 let firstAnswer  = window.prompt('Do you head left or right?')
 
 if(firstAnswer === 'left'){
     let secondAnswer = window.prompt(`You come across a stray cat. 
 It scampers off down a small hole, just large enough for you to crawl through. 
 Do you follow it, or continue on your path?`)
+        if(secondAnswer === 'follow') {
+            let thirdAnswer = window.prompt(`You follow the cat to a colony of cats, nestled in a fort of warm blankets and subsisting off of inexplicably warm soup. They are content with you staying, but you wonder if you should alert the world to this magical safe haven.`)
+        } else if(firstAnswer === 'continue') {
+            let thirdAnswer = window.prompt(`You come across a chamber that reaches upward to a shining light above. There is a long, winding staircase, and a much quicker, but rickety-looking ladder that leads up toward the light. Which do you take?`)
+        }
 
 } else if(firstAnswer === 'right') {
     let secondAnswer = window.prompt(`You come across a snoring dragon. 
