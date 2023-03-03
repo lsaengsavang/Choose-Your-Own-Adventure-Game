9e9931051cc95a7f9a5ee84874306f6677831989
Leeann Saengsavang <leeann.saengsavang@gmail.com>
2023-03-02 17:08:17 -0800

    Added more prompts

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 let firstAnswer  = window.prompt('Do you head left or right?')
 
 if(firstAnswer === 'left'){
@@ -7,7 +6,13 @@ It scampers off down a small hole, just large enough for you to crawl through.
 Do you follow it, or continue on your path?`)
         if(secondAnswer === 'follow') {
             let thirdAnswer = window.prompt(`You follow the cat to a colony of cats, nestled in a fort of warm blankets and subsisting off of inexplicably warm soup. They are content with you staying, but you wonder if you should alert the world to this magical safe haven.`)
-        } else if(firstAnswer === 'continue') {
+            if(thirdAnswer === 'stay') {
+                let fourthAnswer = window.prompt(`You live happily amongst the cats for the rest of your days.`)
+            } else if(thirdAnswer === 'spread the word') {
+                let fourthAnswer = window.prompt(`After leaving the cat colony, you are never able to find it again; without proof, no one believes your story, which passes into legend nonetheless.`)
+            }
+
+        } else if(secondAnswer === 'continue') {
             let thirdAnswer = window.prompt(`You come across a chamber that reaches upward to a shining light above. There is a long, winding staircase, and a much quicker, but rickety-looking ladder that leads up toward the light. Which do you take?`)
         }
 
